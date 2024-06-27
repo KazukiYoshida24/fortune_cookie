e787ffa916bf8a7a97a1baab3daa24947b010ff0
Kazuki <ky882320@gmail.com>
2024-06-27 09:53:49 +0900

    Add ElevatedButton and Restructured Widget

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  String _currentFortune = "";
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _currentFortune = "";
+
+  final _fortuneList = [
+    "大吉：すごくいい",
+    "吉：いい",
+    "中吉：まあいい",
+    "小吉：ほんの少しいい",
+    "末吉：普通",
+    "凶：クソ",
+    "大凶：とてもクソ",
+  ];
+
+  void _randomFortune() {
+    var random = Random();
+    int fortune = random.nextInt(_fortuneList.length);
+    setState(() {
+      //これによりアプリのディスプレイに表示される
+      _currentFortune = _fortuneList[fortune];
+    });
+
+    print(_currentFortune);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Text("Other things/."),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '${_currentFortune}',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: _randomFortune, child: Text('Get Fortune'))
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _randomFortune,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
